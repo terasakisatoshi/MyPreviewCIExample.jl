4f9007e23d3b2ac481acde248b82729d4810dd99
Documenter.jl <documenter@juliadocs.github.io>
2024-03-05 04:23:46 +0000

    delete history

---
+++ previews/PR8/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyPreviewCIExample","category":"page"},{"location":"#MyPreviewCIExample","page":"Home","title":"MyPreviewCIExample","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyPreviewCIExample.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyPreviewCIExample]","category":"page"},{"location":"","page":"Home","title":"Home","text":"Hello World!","category":"page"}]
+}
